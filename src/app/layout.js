76fbfb658d40b5d4ef814b5eb3aff4b6b19dcecb
Andrew Kennedy <andrew.kennedy@mandogroup.com>
2024-08-08 16:08:57 +0100

    order of login prompt & header

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,8 +10,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en-GB">
       <body>
-        <LoginPrompt />
         <Header />
+        <LoginPrompt />
         {children}
         <Footer />
       </body>
